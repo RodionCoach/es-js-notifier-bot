e3df9e5ae61e59a0839a577acaa544b802b8e199
Rodion <rodion.kochetkov@effective-soft.com>
2019-10-04 11:50:15 +0300

    heroku deploy

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import * as functions from 'firebase-functions';
-import Telegraf from'telegraf';
+import Telegraf from 'telegraf';
 
-const bot = new Telegraf(functions.config().bot.token);
+const bot = new Telegraf(`${process.env.BOT_TOKEN}`);
 bot.start((ctx: any) => ctx.reply('Welcome'));
 bot.help((ctx: any) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx: any) => ctx.reply('👍'));
 bot.hears('hi', (ctx: any) => ctx.reply('Hey there'));
 bot.launch().catch(er => {throw new Error(er.message)});
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send();
-});
