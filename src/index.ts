1d1d1fbc829c9f2e756fa44d3ebce32732bf0bd4
Rodion <rodion.kochetkov@effective-soft.com>
2019-10-04 12:04:03 +0300

    some changes

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import Telegraf from 'telegraf';
-
-const bot = new Telegraf(`${process.env.BOT_TOKEN}`);
-bot.start((ctx: any) => ctx.reply('Welcome'));
-bot.help((ctx: any) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx: any) => ctx.reply('👍'));
-bot.hears('hi', (ctx: any) => ctx.reply('Hey there'));
-bot.launch().catch(er => {throw new Error(er.message)});
