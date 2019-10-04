b8a15abc7c7cf0ba05e4b9f93746b92d17065d57
Rodion <rodion.kochetkov@effective-soft.com>
2019-10-04 14:22:39 +0300

    some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Telegraf = require('telegraf');
 
 const bot = new Telegraf('920827169:AAE-8CDdbv7-RGfd_QPsRNX62M4-7T0WEH0');
 bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.ventilate((ctx) => ctx.reply('Проветрюли!'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch().catch((err) => {throw new Error(err.message)});
