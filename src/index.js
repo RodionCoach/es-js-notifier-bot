76438a26eec384e5c5552a25e9b0ddfa28b9ff0a
Rodion <rodion.kochetkov@effective-soft.com>
2019-10-04 12:24:23 +0300

    got a bot token

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf('920827169:AAE-8CDdbv7-RGfd_QPsRNX62M4-7T0WEH0');
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
