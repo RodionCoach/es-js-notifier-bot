e76d864ed8fab45f36b5d41168564c19f2ee762a
Rodion <rodion.kochetkov@effective-soft.com>
2019-10-10 21:25:51 +0300

    try without starWebhook

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ if (process.env.ENV === 'prod') {
   app.use((ctx, next) => (ctx.method === 'POST' || ctx.url === '/'
     ? bot.handleUpdate(ctx.request.body, ctx.response)
     : next()));
-  bot.startWebhook('https://es-js-notifier-bot.herokuapp.com/', process.env.PORT);
   app.listen(process.env.PORT);
 } else if (process.env.ENV === 'dev') {
   bot.launch().catch((err) => { throw new Error(err.message); });
