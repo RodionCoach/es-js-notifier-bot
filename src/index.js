2dcbb08896803b31ec4180f493ea2f58e3e9da57
Rodion <rodion.kochetkov@effective-soft.com>
2019-10-11 19:33:48 +0300

    fix bot run

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const bot = require('./telegram');
 
 if (process.env.ENV === 'prod') {
   bot.telegram.setWebhook(process.env.WEBHOOK_PATH);
-
   const app = new Koa();
   app.use(koaBody());
   app.use((ctx, next) => (ctx.method === 'POST' || ctx.url === '/'
     ? bot.handleUpdate(ctx.request.body, ctx.response)
     : next()));
   app.listen(process.env.PORT);
+  bot.telegram.startWebhook(process.env.WEBHOOK_PATH);
 } else if (process.env.ENV === 'dev') {
   bot.launch().catch((err) => { throw new Error(err.message); });
 }
