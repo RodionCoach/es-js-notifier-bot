3eef536b3aad612091ae0746938de6275124e845
Rodion <rodion.kochetkov@effective-soft.com>
2019-10-11 19:41:33 +0300

    any parameters

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ const koaBody = require('koa-body');
 const bot = require('./telegram');
 
 if (process.env.ENV === 'prod') {
+  console.log(process.env.ENV);
   bot.telegram.setWebhook(process.env.WEBHOOK_PATH);
   const app = new Koa();
   app.use(koaBody());
-  app.use((ctx, next) => (ctx.method === 'POST' || ctx.url === '/'
+  app.use((ctx, next) => (ctx.method === 'POST' || ctx.url === '/*'
     ? bot.handleUpdate(ctx.request.body, ctx.response)
     : next()));
   app.listen(process.env.PORT);
-  bot.telegram.startWebhook(process.env.WEBHOOK_PATH);
 } else if (process.env.ENV === 'dev') {
+  console.log(process.env.ENV);
   bot.launch().catch((err) => { throw new Error(err.message); });
 }
