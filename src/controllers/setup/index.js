11dbc9f7a6ee81a8b28b0e26c69c37d9d36ce038
Rodion <rodion.kochetkov@effective-soft.com>
2019-10-14 12:49:24 +0300

    hard set the bot mode and the occur time

--- src/controllers/setup/index.js
+++ src/controllers/setup/index.js
@@ -1,21 +1,17 @@
-const session = require('telegraf/session');
-const { keqboardChoice } = require('./markup');
 const { isAdmin } = require('../admin/index');
 const { init, data } = require('../../bot_config');
 const { botNotify } = require('../timer/index');
 const scheduleInit = require('../timer/taskInit');
 require('dotenv').config();
 
-const botInit = (bot, stage) => {
+const botInit = (bot) => {
   let running = false;
   const currentTask = {};
 
   try {
-    init(process.env.BOT_MODE, process.env.BOT_INTERVAL, process.env.BOT_OCCUR_TIME);
+    init(process.env.BOT_MODE, process.env.BOT_MODE_TIME, process.env.BOT_OCCUR_TIME);
     bot.start((ctx) => ctx.reply('Welcome'));
     bot.settings((ctx) => ctx.reply(`currently settings: ${JSON.stringify(data.config)}`));
-    bot.command('fresh_air', (ctx) => isAdmin(ctx) && !running && ctx.replyWithPhoto(process.env.FILE_ID));
-    bot.command('setup', (ctx) => isAdmin(ctx) && !running && keqboardChoice(ctx, 'Please choise bot behavior'));
     bot.command('run', (ctx) => {
       if (!running) {
         if (isAdmin(ctx)) {
@@ -30,28 +26,10 @@ const botInit = (bot, stage) => {
         ctx.reply('The bot is running already!');
       }
     });
-    bot.command('run_default', (ctx) => {
-      if (!running) {
-        if (isAdmin(ctx)) {
-          running = true;
-          init(process.env.BOT_MODE, process.env.BOT_INTERVAL, process.env.BOT_OCCUR_TIME);
-          if (!currentTask.notify) {
-            currentTask.notify = scheduleInit(ctx.replyWithPhoto(process.env.FILE_ID));
-          }
-          botNotify(currentTask.notify, 'start');
-          ctx.reply('Bot started!');
-        }
-      } else {
-        ctx.reply('The bot is running already!');
-      }
-    });
     bot.command('stop', (ctx) => {
       if (running) {
         if (isAdmin(ctx)) {
           running = false;
-          if (!currentTask.notify) {
-            currentTask.notify = scheduleInit(ctx.replyWithPhoto);
-          }
           botNotify(currentTask.notify, 'stop');
           ctx.reply('Bot has been Stopped!');
         }
@@ -59,14 +37,6 @@ const botInit = (bot, stage) => {
         ctx.reply('The bot is not running yet!');
       }
     });
-
-    bot.use(session());
-    bot.use(stage.middleware());
-    bot.action('setInterval', (ctx) => isAdmin(ctx) && ctx.scene.enter('setInterval'));
-    bot.action('setTime', (ctx) => isAdmin(ctx) && ctx.scene.enter('setTime'));
-    bot.action('setMode', (ctx) => isAdmin(ctx) && ctx.scene.enter('setMode'));
-    bot.action('setModeInterval', () => { data.config.mode = process.env.BOT_MODE_INTERVAL; });
-    bot.action('setModeTime', () => { data.config.mode = process.env.BOT_MODE_TIME; });
   } catch (error) {
     console.log(error);
   }
--- src/controllers/timer/taskInit.js
+++ src/controllers/timer/taskInit.js
@@ -3,36 +3,13 @@ const { data } = require('../../bot_config'); // better to require it or to put 
 require('dotenv').config();
 
 const scheduleInit = (task) => {
-  let configuredTask;
-  const interval = data.config.interval.split(':');
   const time = data.config.time.split(':');
-
-  switch (data.config.mode) {
-    case process.env.BOT_MODE_INTERVAL:
-      configuredTask = cron.schedule(`*/${+interval[1] || ''} */${+interval[0] || ''} * * *`, () => {
-        task(process.env.FILE_ID);
-      }, {
-        scheduled: false,
-      });
-      break;
-    case process.env.BOT_MODE_TIME:
-      configuredTask = cron.schedule(`${time[1]} ${time[0]} * * *`, () => {
-        task(process.env.FILE_ID);
-      }, {
-        scheduled: false,
-        timezone: process.env.TIME_ZONE,
-      });
-      break;
-    default:
-      configuredTask = cron.schedule(`*/${+interval[1] || ''} */${+interval[0] || ''} * * *`, () => {
-        task(process.env.FILE_ID);
-        console.log('bot running by default');
-      }, {
-        scheduled: false,
-      });
-      break;
-  }
-  return configuredTask;
+  return cron.schedule(`${time[1]} ${time[0]} * * *`, () => {
+    task(process.env.FILE_ID);
+  }, {
+    scheduled: false,
+    timezone: process.env.TIME_ZONE,
+  });
 };
 
 module.exports = scheduleInit;
